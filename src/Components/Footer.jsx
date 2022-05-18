06c58ef3ea14eceb6407073c078d9874336994e3
Lukas-i-bastun <lukash@live.se>
2022-05-18 09:08:48 +0200

    all done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,17 +5,29 @@ import {BsInstagram, BsGithub} from 'react-icons/bs'
 function Footer() {
   return (
     <section id='footer-section'>
-        <div className='info-div'>
-            <a >Phone: +46 738 546 407</a>
-            <a >Email: HagbergDevelopment@gmail.com</a>
+
+          <div className='icons-div'>
+            <a target={'_blank'} href="https://www.facebook.com/lukas.hagberg.9/">
+            <AiFillFacebook className='footer-icon'/>
+            </a>
+           <a target={'_blank'} href="https://www.instagram.com/lukas__hagberg/">
+           <BsInstagram className='footer-icon'/> 
+           </a>
+            <a target={'_blank'} href="https://github.com/lusweek">
+            <BsGithub className='footer-icon'/>
+            </a>
+            <a target={'_blank'} href="https://www.linkedin.com/in/lukas-hagberg-6855441a6/">
+            <AiFillLinkedin className='footer-icon'/>
+            </a>
             
         </div>
-        <div className='icons-div'>
-            <AiFillFacebook className='footer-icon'/>
-            <BsInstagram className='footer-icon'/> 
-            <BsGithub className='footer-icon'/>
-            <AiFillLinkedin className='footer-icon'/>
+
+        <div className='info-div'>
+            <a href="tel:+46738546407">Phone: +46 738 546 407</a>
+            <a href="mailto:HagbergDevelopment@gmail.com" >Email: HagbergDevelopment@gmail.com</a>
+            
         </div>
+       
         
 
     </section>
