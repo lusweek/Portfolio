feb963c658c6d38b974ac396035791f8a2d29372
Lukas-i-bastun <lukash@live.se>
2022-10-16 15:44:51 +0200

    upates

--- src/Components/ServiceSection/ServiseSection.jsx
+++ src/Components/ServiceSection/ServiseSection.jsx
@@ -11,21 +11,14 @@ import SWAG_IMG from '../../media/img/swagimg.png'
 import BRAKING_BAD_IMG from '../../media/img/brakingBad.png'
 import NERD_INFO_IMG from '../../media/img/nerd-info.png'
 import BRÖLLOPSFOTOGRAFEN_IMG from '../../media/img/pwaApp.webp'
+import ADRIAN_WEBPAGE_IMG from '../../media/img/adrian.png'
  
 function ServiseSection() {
 
 
   // ADD NEW OBJECT HERE
-  const cards = [
-    {
-      title: 'Snake Game',
-      cardText: 'I created\nthe snake game with simple javaScript and a grid.',
-      modalText: `I made a simple snake game with the rule that the snake goes faster for exery food it eats`,
-      img: SNAKE_IMG ,
-      icon: <GiSandSnake />,
-      githubLink: 'https://github.com/lusweek/SnakeGame.git',
-      linkText: 'link to github:',
-    },
+  // Förra hetter cards
+  const skolProjekt = [
     {
       title: 'Recipe app', 
       cardText: `An app capable of searching for any food from a Recipe API and seing the Recipe for any food. `,
@@ -36,25 +29,7 @@ function ServiseSection() {
       linkText: 'link to github:',
 
     }, 
-    {
-      title: "SWAG's webpage", 
-      cardText: `This is the first project I ever did on my own. SWAG's website`,
-      modalText: `I started working on this project after studying front-end for about 3 months, I was so eager to use the things I learned and to get started. There has bin many times I'v bin thinking of redo this project, the code is very messy and I would create it in a whole new way if I redo it. But still, it was very fun to play around with this project and I learned a lot building it.`,
-      img: SWAG_IMG ,
-      icon: <GiMuscleUp />,
-      githubLink: 'https://lusweek.github.io/swag/',
-      linkText: 'link to webpage:'
-    },  
-    {
-      title: "Braking Bad API", 
-      cardText: `An app using fetch, advanced css and a search function`,
-      modalText: `This simple API app uses the fetch function to get all the characters from the Braking Bad series and then using .map to create a card for every character. Hovering over any card flips the card and shows some info bout that character. It was realy fun to learn how to create this flipping thing using css. There is a search feald you can use to search for any character you want.`,
-      img: BRAKING_BAD_IMG,
-      icon: <MdMovieFilter />,
-      githubLink: 'https://github.com/lusweek/Braking-bad-API.git',
-      linkText: 'link to github:',
-
-    },  
+   
     {
       title: "Star wars API", 
       cardText: `A very simpe API app using the fetch method to get data from SWAPI`,
@@ -75,39 +50,75 @@ function ServiseSection() {
       linkText: 'link to webpage:',
 
     },  
+
+  ]
+
+
+  const egnaProjekt = [
     {
       title: "Adrians webpage", 
-      cardText: `A one page website, designed it myself using figma.`,
-      modalText: `A one page website for a friend. Built in react, designed it myself using figma.`,
-      img: null,
+      cardText: `A one page website I build for my friend who is a personal trainer`,
+      modalText: `Build completly from scratch. Designed with figma based on an intervjew and agile work with Adrian, the design changed multiple times. The purpose of this website is for Adrian to get started as a persinal trainer.`,
+      img: ADRIAN_WEBPAGE_IMG,
       icon: <CgWebsite />,
       githubLink: 'https://lusweek.github.io/Adrians-hemsida/',
       linkText: 'link to webpage:',
-
     },  
-
-
-    
-  
+    {
+      title: "SWAG's webpage", 
+      cardText: `This is the first project I ever did on my own. SWAG's website`,
+      modalText: `I started working on this project after studying front-end for about 3 months, I was so eager to use the things I learned and to get started. There has bin many times I'v bin thinking of redo this project, the code is very messy and I would create it in a whole new way if I redo it. But still, it was very fun to play around with this project and I learned a lot building it.`,
+      img: SWAG_IMG ,
+      icon: <GiMuscleUp />,
+      githubLink: 'https://swag-temp.pages.dev/',
+      linkText: 'link to webpage:'
+    },  
+    {
+      title: "Braking Bad API", 
+      cardText: `An app using fetch, advanced css and a search function`,
+      modalText: `This simple API app uses the fetch function to get all the characters from the Braking Bad series and then using .map to create a card for every character. Hovering over any card flips the card and shows some info bout that character. It was realy fun to learn how to create this flipping thing using css. There is a search feald you can use to search for any character you want.`,
+      img: BRAKING_BAD_IMG,
+      icon: <MdMovieFilter />,
+      githubLink: 'https://github.com/lusweek/Braking-bad-API.git',
+      linkText: 'link to github:',
+    },  
+    {
+      title: 'Snake Game',
+      cardText: 'I created\nthe snake game with simple javaScript and a grid.',
+      modalText: `I made a simple snake game with the rule that the snake goes faster for exery food it eats`,
+      img: SNAKE_IMG ,
+      icon: <GiSandSnake />,
+      githubLink: 'https://snake-3i5.pages.dev/',
+      linkText: 'Try it ouy here!',
+    },
   ]
 
-  const [isOpen, setIsOpen] = useState(false)
-  const [clickedCard, setClickedCard] = useState(0)
+  const [isOpenSkol, setIsOpenSkol] = useState(false)
+  const [clickedCardEgnaProjekt, setClickedCardEgnaProjekt] = useState(0)
 
-  const openModal = (index) => {
-    setClickedCard(index)
-    setIsOpen(true)
+  const [isOpenEgna, setIsOpenEgna] = useState(false)
+  const [clickedCardSkolprojekt, setClickedCardSkolprojekt] = useState(0)
+
+
+  const openModalEgnaProjekt = (index) => {
+    setClickedCardEgnaProjekt(index)
+    setIsOpenEgna(true)
+  }
+
+  const openModalSkolprojekt = (index) => {
+    setClickedCardSkolprojekt(index)
+    setIsOpenSkol(true)
   }
 
   return (
     <>
-        <h1 id='service-title' className='large-title'>Projects</h1>
+        <h1 id='service-title' className='large-title'>Sidoprojekt</h1>
     <section id='service-section'>
 
-      {cards.map((card, index) =>  {
+      {egnaProjekt.map((card, index) =>  {
         return(
           <Card 
-            setIsOpen={() => openModal(index)}
+            setIsOpen={() => openModalEgnaProjekt(index)}
             icon = {card.icon}
             title = {card.title}
             cardText = {card.cardText}
@@ -116,18 +127,48 @@ function ServiseSection() {
       })}
 
         <ServiceModal 
-        isOpen={isOpen} 
-        setIsOpen={() => setIsOpen(false)}
-        icon = {cards[clickedCard].icon}
-        title = {cards[clickedCard].title}
-        img = {cards[clickedCard].img}
-        modalText = {cards[clickedCard].modalText}
-        githubLink = {cards[clickedCard].githubLink}
-        linkText = {cards[clickedCard].linkText}
+        isOpen={isOpenEgna} 
+        setIsOpen={() => setIsOpenEgna(false)}
+        icon = {egnaProjekt[clickedCardEgnaProjekt].icon}
+        title = {egnaProjekt[clickedCardEgnaProjekt].title}
+        img = {egnaProjekt[clickedCardEgnaProjekt].img}
+        modalText = {egnaProjekt[clickedCardEgnaProjekt].modalText}
+        githubLink = {egnaProjekt[clickedCardEgnaProjekt].githubLink}
+        linkText = {egnaProjekt[clickedCardEgnaProjekt].linkText}
 
          />
+
+    </section>
+
+
+    <h1 id='service-title' className='large-title'>Sidoprojekt</h1>
+    <section id='service-section'>
+
+    {skolProjekt.map((card, index) =>  {
+        return(
+          <Card 
+            setIsOpen={() => openModalSkolprojekt(index)}
+            icon = {card.icon}
+            title = {card.title}
+            cardText = {card.cardText}
+        /> 
+        )
+      })}
+
+        <ServiceModal 
+        isOpen={isOpenSkol} 
+        setIsOpen={() => setIsOpenSkol(false)}
+        icon = {skolProjekt[clickedCardSkolprojekt].icon}
+        title = {skolProjekt[clickedCardSkolprojekt].title}
+        img = {skolProjekt[clickedCardSkolprojekt].img}
+        modalText = {skolProjekt[clickedCardSkolprojekt].modalText}
+        githubLink = {skolProjekt[clickedCardSkolprojekt].githubLink}
+        linkText = {skolProjekt[clickedCardSkolprojekt].linkText}
+
+        />
         
     </section>
+
     </>
   )
 }
